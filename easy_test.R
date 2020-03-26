ae0b3d4de9fb87774278640113673561277fe312
AleksandraSteiner <44055251+AleksandraSteiner@users.noreply.github.com>
2020-03-26 14:29:31 +0100

    adds easy_test file

---
+++ easy_test.R
@@ -0,0 +1,15 @@
+#EASY TEST
+
+devtools::install_github("jolars/SLOPE")
+library("SLOPE")
+
+X <- abalone[[1]]
+Y <- abalone[[2]]
+
+SLOPE_model <- SLOPE(X, Y, lambda = "bh")
+
+lambda <- rep(0.5, 9)
+Lasso_model <- SLOPE(X, Y, lambda = lambda)
+
+plot(SLOPE_model)
+plot(Lasso_model)
